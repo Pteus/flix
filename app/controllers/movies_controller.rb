8b4b62424fc1e115fd871ef15ec02e012d4d6bbe
Pedro Mateus <pedropmateus91@gmail.com>
2025-10-07 00:06:25 +0100

    Edit, Create, partials

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,39 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [ :show, :edit, :update ]
+
   def index
     @movies = Movie.all
   end
 
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    @movie.update(movie_params)
+    redirect_to @movie
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+
+    redirect_to @movie
+  end
+
+
+  private
+  def movie_params
+    params.expect(movie: [ :title, :description, :rating, :released_on, :total_gross ])
+  end
+
+  def set_movie
     @movie = Movie.find(params[:id])
   end
 end
