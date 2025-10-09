b7dfc8204763a6710aca10a68ff511faa36273d6
Pedro Mateus <pedropmateus91@gmail.com>
2025-10-09 21:37:26 +0100

    improve validation for `email`

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true, format: { with: /\S+@\S+/ },
+  validates :email, presence: true, format: { with: /\A\S+@\S+\Z/ },
             uniqueness: { case_sensitive: false }
 end
