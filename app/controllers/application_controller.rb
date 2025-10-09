cb9e52c1df1ed4d37a87c9468d5463e3ad8003e9
Pedro Mateus <pedropmateus91@gmail.com>
2025-10-09 23:46:37 +0100

    Authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   add_flash_types :danger
+
+  private
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+  end
 end
